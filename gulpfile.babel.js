570236ca4f15a623a40b3196f51f659b59f64315
lyfnsmile <li.zhixiang@live.cn>
2016-12-22 22:13:23 +0800

    update

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,6 +7,11 @@ import autoprefixer from 'gulp-autoprefixer';
 import notify from 'gulp-notify';
 import browserSync from 'browser-sync';
 import eslint from 'gulp-eslint'
+import concat from 'gulp-concat'
+import rev from 'gulp-rev'
+import uglify from 'gulp-uglify';
+import del from 'del';
+
 
 var reload = browserSync.reload;
 
@@ -17,11 +22,21 @@ gulp.task('eslint', () => {
     .pipe(eslint.failAfterError());
 });
 
+
+gulp.task('clean', () =>{
+  return del('dist/**').then(paths => {
+    console.log('Deleted files and folders:\n', paths.join('\n'));
+  });
+});
+
+
 gulp.task('sass', () => {
   gulp.src('src/assets/styles/*.scss')
     .pipe(sass())
     .pipe(autoprefixer())
-    .pipe(gulp.dest('dist'))
+    .pipe(concat('app.css'))
+    .pipe(rev())
+    .pipe(gulp.dest('dist/style'))
     .pipe(browserSync.reload({ stream: true }))
     .pipe(notify({ message: 'sass task complete' }));
 })
@@ -29,10 +44,10 @@ gulp.task('sass', () => {
 gulp.task('babel', () => {
   gulp.src('src/app/**/*.js')
     .pipe(babel())
-    .pipe(gulp.dest('dist'))
+    .pipe(concat('app.js'))
+    .pipe(rev())
+    .pipe(gulp.dest('dist/js'))
     .pipe(notify({ message: 'babel task complete' }));
-
-
 
 })
 
@@ -45,7 +60,7 @@ gulp.task('serve', ['sass', 'babel'], () => {
     }
   });
 
-  gulp.watch("*.html").on('change', browserSync.reload);
+  gulp.watch("src/**/*.html").on('change', browserSync.reload);
   gulp.watch('*.scss', ['sass']);
   gulp.watch("src/app/*.js", ['js-watch']);
 });
@@ -53,6 +68,12 @@ gulp.task('serve', ['sass', 'babel'], () => {
 gulp.task('default', ['serve']);
 
 gulp.task('watch', () => {
+  gulp.watch('src/assets/styles/*.scss', ['sass']);
+  gulp.watch('src/app/**/*.js', ['babel']);
+})
+
+
+gulp.task('build', () => {
   gulp.watch('src/assets/styles/*.scss', ['sass']);
   gulp.watch('src/app/**/*.js', ['babel']);
 })
